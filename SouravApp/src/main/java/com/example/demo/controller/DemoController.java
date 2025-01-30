58a6742de43fdda7a05fa5349931eefb8b7abb1e
routomm2000 <routomm2000@gmail.com>
2025-01-30 11:34:20 +0530

    Add files via upload

---
+++ SouravApp/src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.service.DemoService;
+
+@RestController
+@RequestMapping("demo")
+public class DemoController {
+	
+	@Autowired
+	private DemoService DemoService;
+	
+	
+}
