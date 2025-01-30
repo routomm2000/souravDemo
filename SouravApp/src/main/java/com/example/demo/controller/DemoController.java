dc4e7fd2e90576551e00d067a23eb988bd918173
routomm2000 <routomm2000@gmail.com>
2025-01-30 11:43:28 +0530

    changing end point name

--- SouravApp/src/main/java/com/example/demo/controller/DemoController.java
+++ SouravApp/src/main/java/com/example/demo/controller/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.service.DemoService;
 
 @RestController
-@RequestMapping("demo")
+@RequestMapping("demo1")
 public class DemoController {
 	
 	@Autowired
